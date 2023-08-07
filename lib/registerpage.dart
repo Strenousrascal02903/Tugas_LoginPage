5f96ee42b9ae9f683845934fdab5d2832496c5b7
Strenousrascal02903 <dahlah0108@gmail.com>
2023-08-07 11:04:37 +0700

    Feat: [NA] registerpage

--- lib/registerpage.dart
+++ lib/registerpage.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class loginPage extends StatelessWidget {
-  const loginPage({super.key});
+class registerPage extends StatelessWidget {
+  const registerPage({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -22,7 +22,7 @@ class loginPage extends StatelessWidget {
                   margin: EdgeInsets.all(10),
                   padding: EdgeInsets.all(10),
                   decoration: BoxDecoration(
-                      color: Colors.white,
+                      color: Color(int.parse("0xff757DC6")),
                       borderRadius: BorderRadius.all(Radius.circular(50))),
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
@@ -31,14 +31,15 @@ class loginPage extends StatelessWidget {
                           width: 140,
                           height: 30,
                           decoration: BoxDecoration(
-                              color: Color(int.parse("0xff757DC6")),
+                              color: Colors.white,
                               borderRadius:
                                   BorderRadius.all(Radius.circular(30))),
                           child: Center(
                             child: Text(
-                              "LOGIN",
-                              style:
-                                  TextStyle(color: Colors.white, fontSize: 18),
+                              "REGISTER",
+                              style: TextStyle(
+                                  color: Color(int.parse("0xff757DC6")),
+                                  fontSize: 18),
                             ),
                           )),
                       Container(
@@ -46,13 +47,11 @@ class loginPage extends StatelessWidget {
                         width: 350,
                         height: 40,
                         decoration: BoxDecoration(
-                            color: Color(int.parse("0xff757DC6")),
+                            color: Colors.white,
                             borderRadius:
                                 BorderRadius.all(Radius.circular(30))),
                         child: TextField(
-                          style: TextStyle(
-                              color: Color(int.parse("0xff26295E")),
-                              fontSize: 14),
+                          style: TextStyle(color: Colors.white, fontSize: 14),
                           decoration: InputDecoration(
                               labelText: "Nama",
                               hintText: "masukkan nama",
@@ -76,7 +75,7 @@ class loginPage extends StatelessWidget {
                         width: 350,
                         height: 40,
                         decoration: BoxDecoration(
-                            color: Color(int.parse("0xff757DC6")),
+                            color: Colors.white,
                             borderRadius:
                                 BorderRadius.all(Radius.circular(30))),
                         child: TextField(
@@ -107,7 +106,7 @@ class loginPage extends StatelessWidget {
                         width: 350,
                         height: 40,
                         decoration: BoxDecoration(
-                            color: Color(int.parse("0xff757DC6")),
+                            color: Colors.white,
                             borderRadius:
                                 BorderRadius.all(Radius.circular(30))),
                         child: TextField(
@@ -142,10 +141,13 @@ class loginPage extends StatelessWidget {
                                 EdgeInsets.only(bottom: 43, top: 20, right: 10),
                             child: Expanded(
                               child: ElevatedButton(
-                                onPressed: () {},
+                                onPressed: () {
+                                  Navigator.pushNamed(context, "loginpage");
+                                },
                                 style: ElevatedButton.styleFrom(
                                     minimumSize: Size(150, 40),
-                                    backgroundColor:
+                                    backgroundColor: Colors.white,
+                                    foregroundColor:
                                         Color(int.parse("0xff757DC6"))),
                                 child: Text(
                                   "Login",
@@ -159,13 +161,10 @@ class loginPage extends StatelessWidget {
                                 EdgeInsets.only(bottom: 43, top: 20, left: 10),
                             child: Expanded(
                               child: ElevatedButton(
-                                onPressed: () {
-                                  Navigator.pushNamed(context, "registerpage");
-                                },
+                                onPressed: () {},
                                 style: ElevatedButton.styleFrom(
                                     minimumSize: Size(150, 40),
-                                    backgroundColor: Colors.white,
-                                    foregroundColor:
+                                    backgroundColor:
                                         Color(int.parse("0xff757DC6"))),
                                 child: Text(
                                   "Register",
